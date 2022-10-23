5c203d3cb4cb04ea3f63ed16edeaf3fd30e1d15b
andrey <a.azeev1c@yandex.ru>
2022-10-23 20:12:40 +0300

    #1002

--- app.py
+++ app.py
@@ -1,43 +1,51 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 import json
 import os
+from environs import Env
 
+env = Env()
+env.read_env()
+
+login = env.str('login')
+password = env.str('password')
 
 name_file_settings = 'data_settings.json'
 
 
 def main(data_dict_json):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://auth.kontur.ru/")
-    sleep(5)
+    sleep(3)
     elem = driver.find_element(by=By.CLASS_NAME, value='Tabs__star_2V_I')
     # elem = driver.find_element(by=By.NAME, value='login')
     elem.click()
     sleep(2)
-    elems = driver.find_elements(by=By.CLASS_NAME, value='react-ui-123wljp')
-    el_c = 1
-    for elem in elems:
-        if el_c == 3: break
-        elem.send_keys(login if el_c == 1 else password)
-        el_c += 1
-    sleep(2)
+    elem = driver.find_element(by=By.NAME, value='login')
+    elem.send_keys(login)
+    elem = driver.find_element(by=By.CSS_SELECTOR, value='[type="password"]')
+    elem.send_keys(password)
 
     driver.find_element(By.TAG_NAME, 'button').click()
-    sleep(10)
+    sleep(3)
     driver.get('https://diadoc.kontur.ru/')
-    sleep(5)
-    number = 0;
-    while number == 0:
-        input()
-    # elem_new_doc = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[3]/div[1]/div/div[1]/a/span[2]')
-    # elem_new_doc.click()
-    # sleep(3)
+    sleep(3)
 
-    elem_upd = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[4]/div/div[2]/div[1]/div[2]/div[2]/span[4]/button/div/span')
+    # number = 0;
+    # while number == 0:
+    #     input()
+    # elem_new_doc = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[3]/div[1]/div/div[1]/a/span[2]')
+    elem_new_doc = driver.find_element(By.LINK_TEXT, 'Новый документ')
+    elem_new_doc.click()
+    sleep(3)
+
+    # elem_upd = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[4]/div/div[2]/div[1]/div[2]/div[2]/span[4]/button/div/span')
+    elem_upd = driver.find_element(By.XPATH, '/html/body/div[7]/div[2]/div/div[4]/div/div[2]/div[1]/div[2]/div[2]/span[4]/button/div/span')
     elem_upd.click()
     sleep(3)
     driver.switch_to.window(driver.window_handles[1])
@@ -67,6 +75,13 @@ def main(data_dict_json):
         for i in range(1, len(data_dict_json)):
             elem_new_row.click()
 
+    print("Нужно добавить колонку с артикулом / кодом товара")
+
+    while True:
+        stop_number = input('Для продолжение введите 0 :')
+        if stop_number == '0':
+            break
+
     elems_row = driver.find_elements(By.CLASS_NAME, '_2iI1P')
     i = data_json.get('start_position_first_row', 0)
     cont_pix = data_json.get('cont_pix', 0)
@@ -89,10 +104,13 @@ def main(data_dict_json):
             detail[1].find_element(By.TAG_NAME, "input").send_keys(data_dict_json[iter].get('count'))
             detail[2].find_element(By.TAG_NAME, "input").send_keys(data_dict_json[iter].get('price'))
 
-            name_barcode = elem_r.find_element(By.CLASS_NAME, "pthlf")
-            t_bar = name_barcode.find_elements(By.CLASS_NAME, "_2t_I2")
-            t_bar[0].find_element(By.TAG_NAME, "input").send_keys("шк")
-            t_bar[1].find_element(By.TAG_NAME, "input").send_keys(f"{data_dict_json[iter].get('barcode')}")
+            vendor_code = elem_r.find_element(By.CSS_SELECTOR, value='[data-tid="VendorCodeCell"]')
+            input_vendor_code = vendor_code.find_element(By.TAG_NAME, value='input')
+            input_vendor_code.send_keys(f"{data_dict_json[iter].get('barcode')}")
+            # name_barcode = elem_r.find_element(By.CLASS_NAME, "pthlf")
+            # t_bar = name_barcode.find_elements(By.CLASS_NAME, "_2t_I2")
+            # t_bar[0].find_element(By.TAG_NAME, "input").send_keys("шк")
+            # t_bar[1].find_element(By.TAG_NAME, "input").send_keys(f"{data_dict_json[iter].get('barcode')}")
 
         except Exception as error:
             print(error)
@@ -106,7 +124,8 @@ def main(data_dict_json):
 
 def go_to_button_add(driver: webdriver, position: int) -> webdriver:
     driver.execute_script(f"window.scrollTo(0, {position})")
-    elem_new_row = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/span/div[2]/div[1]/div[8]/div/div/div[1]/div[2]/div/div/div[1]/div/div/div[1]/div/div/div[2]/div/span[2]')
+    # elem_new_row = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/span/div[2]/div[1]/div[8]/div/div/div[1]/div[2]/div/div/div[1]/div/div/div[1]/div/div/div[2]/div/span[2]')
+    elem_new_row = driver.find_element(By.XPATH, '/html/body/div[7]/div[1]/div/span/div[2]/div[1]/div[8]/div/div/div[1]/div[2]/div/div/div[1]/div/div/div[1]/div/div/div[2]/div/span[2]/a')
     return elem_new_row
 
 
@@ -128,3 +147,6 @@ if __name__ == '__main__':
             main(data_dict_json)
     except Exception as error:
         print(error)
+
+# Версии драйверов
+# https://chromedriver.chromium.org/downloads
